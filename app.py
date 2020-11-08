63e800b0b16c49beb3ee00dd10e335dbd0d3db5a
Suprabhat Gurrala <supragurrala@gmail.com>
2020-11-08 13:39:22 -0500

    barebones working app

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from wtforms import StringField, SubmitField
 
 from maxdiff import MaxDiffRater
 
 app = Flask(__name__)
 
+mdr = MaxDiffRater()
+
 @app.route('/')
 def index():
-    mdr = MaxDiffRater()
     sample = mdr.get_sample()
+
+    best = request.args.get("best")
+    worst = request.args.get("worst")
     
-    return render_template('index.html', sample=sample["items"].values)
+    if best is not None and worst is not None:
+        mdr.submit_results(sample.index, best, worst)
+
+    return render_template('index.html', sample=sample["items"])
+
+@app.route('/results')
+def results():
+    df = mdr.elo_ratings.sort_values(["elo", "matches"], ascending=False)
+
+    return render_template('results.html', table=df.to_html(classes=["table", "table-dark", "table-hover"], index=False))
--- maxdiff.py
+++ maxdiff.py
@@ -18,82 +18,56 @@ class MaxDiffRater:
             self.elo_ratings = elo_ratings
         self.k_factor = k_factor
         
-
     def initialize_items(self):
         self.elo_ratings = pd.read_csv("items.csv", names=["items"])
         self.elo_ratings["elo"] = 1500
         self.elo_ratings["matches"] = 0
         self.store_elo_ratings()
 
-
     def store_elo_ratings(self):
         self.elo_ratings.to_csv("elo_ratings.csv", index_label="id")
-
 
     def load_elo_ratings(self):
         self.elo_ratings = pd.read_csv("elo_ratings.csv", index_col="id")
 
-
-    def update_elo(self, elo_ratings, winner, loser):
+    def update_elo(self, winner, loser):
         print(f"Updating elo for {winner} over {loser}")
         if winner == loser:
             return
         
-        a = elo_ratings.loc[winner]["elo"]
-        b = elo_ratings.loc[loser]["elo"]
+        a = self.elo_ratings.loc[winner]["elo"]
+        b = self.elo_ratings.loc[loser]["elo"]
         
         expected = 1 / (1 + 10**((b - a) / 400))
         
-        rating_change = k_factor * (1 - expected)
+        rating_change = self.k_factor * (1 - expected)
         
-        elo_ratings.at[winner, "elo"] += rating_change
-        elo_ratings.at[loser, "elo"] -= rating_change
+        self.elo_ratings.at[winner, "elo"] += rating_change
+        self.elo_ratings.at[loser, "elo"] -= rating_change
         
-        elo_ratings.at[winner, "matches"] += 1
-        elo_ratings.at[loser, "matches"] += 1
+        self.elo_ratings.at[winner, "matches"] += 1
+        self.elo_ratings.at[loser, "matches"] += 1
+    
+    def submit_results(self, sample_ids, best, worst):
+        best = int(best)
+        worst = int(worst)
+        for i in sample_ids:
+            if i != best:
+                self.update_elo(best, i)
+            if (i != worst) and (i != best):
+                self.update_elo(i, worst)
         
-        store_elo_ratings(elo_ratings)
-
+        self.store_elo_ratings()
 
     def get_sample(self, bucket_size=5):
-        least_sample = self.elo_ratings.nsmallest(bucket_size, "matches", keep="all")
+        i = 1
+        least_sample = self.elo_ratings.nsmallest(i, "matches", keep="all")
+
+        while least_sample.shape[0] < bucket_size:
+            i += 1
+            least_sample = self.elo_ratings.nsmallest(i, "matches", keep="all")
         
         if least_sample.shape[0] > bucket_size:
             least_sample = least_sample.sample(bucket_size)
         
         return least_sample
-
-    def driver(self):
-        bucket_size = 5
-        print(f"You will be given {bucket_size} items. You will be asked to identify the best and worst item in each bucket. Enter -1 to exit.")
-        input_val = 0
-        while input_val != -1:    
-            contenders = get_sample(bucket_size)
-            
-            display(contenders)
-            print("Enter the index of the best item in this list: ")
-            best = int(input())
-
-            if best == -1:
-                break
-            elif best not in contenders.index:
-                print("Entered index was not one of the options.")
-            
-                
-            print(f"Marking movie {best} as best.")
-            
-            print("Enter the index of the worst item in this list:")
-            worst = int(input())
-            
-            if worst == -1:
-                break
-                
-            print(f"Marking movie {worst} as worst.")
-            
-            for i in contenders.index:
-                if i != best:
-                    update_elo(best, i)
-                if (i != worst) and (i != best):
-                    update_elo(i, worst)
-            elo_ratings.to_csv("elo_ratings.csv", index_label="id")
-            elo_ratings.sort_values("elo", ascending=False)
