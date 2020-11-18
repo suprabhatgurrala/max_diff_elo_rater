4f98c24242c6a8f58c5f03544cb0d1697ff6c6e1
Suprabhat Gurrala <supragurrala@gmail.com>
2020-11-18 15:27:07 -0500

    Handle cases where only one set of radio buttons is submitted, display a relative strength instead of raw elo

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def index():
         sample_ids = request.form.getlist("sample_ids")
         best = request.form.get("best")
         worst = request.form.get("worst")
-        mdr.submit_results(sample_ids, best, worst)
+        if best is not None and worst is not None:
+            mdr.submit_results(sample_ids, best, worst)
     
     sample = mdr.get_sample()
     
@@ -27,5 +28,9 @@ def results():
     Results page, shows ratings of all items.
     """
     df = mdr.elo_ratings.sort_values(["elo", "matches"], ascending=False)
+    df["score"] = (1 / (1 + 10**((1500 - df["elo"]) / 400))) * 100
+    df = df[["items", "score", "matches"]]
 
-    return render_template('results.html', table=df.to_html(classes=["table", "table-dark", "table-hover"], index=False))
+    html_formatted = df.to_html(classes=["table", "table-dark", "table-hover"], index=False, float_format="%.1f")
+
+    return render_template('results.html', table=html_formatted)
